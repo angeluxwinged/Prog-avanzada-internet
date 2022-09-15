386b6265765df76ff1ae07aecea7079b4a443ef5
angeluxwinged <95920979+angeluxwinged@users.noreply.github.com>
2022-09-15 10:07:41 -0600

    Add files via upload

---
+++ index.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title>Accesso al panel</title>
+	<link rel="stylesheet" type="text/css" href="public/css/main.css">
+</head>
+<div id="container">
+	<form>
+		<fieldset>
+			<legend>
+				Datos acceso
+			</legend>
+
+			<label>
+				Email:
+			</label>
+
+			<input type="text" name="">
+
+			<label>
+				Password:
+			</label>
+			<input type="password" name="">
+
+			<button type="submit">
+				Acceder
+			</button>
+		</fieldset>
+	</form>
+</div>
+
+<body>
+
+</body>
+</html>
